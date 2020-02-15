e7ae128a1a5723be0050d70856d008ee2e28326f
Vedant Puri <vedantpuri13@gmail.com>
2020-02-14 21:37:02 -0500

    Create SuperUser

--- README.md
+++ README.md
@@ -4,5 +4,12 @@ Weather App (Klaviyo)
 ## Requirements
 - Python >= 3.6
 
+## Credentials
+```
+Username: kvadmin
+Email: vedantpuri13@gmail.com
+PW: kvweatherapp
+```
+
 ## Steps to Run
 - conda activate kv (Personal)
--- weatherapp/signup/models.py
+++ weatherapp/signup/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Subscriber(models.Model):
+    email = models.EmailField(max_length=100, blank=True, help_text="Enter your email")
+    Location = models.CharField(max_length=120, blank=True, help_text="Enter your location")
+
+    def __str__(self):
+        return '{0}, {1}'.format(self.email, self.location)
