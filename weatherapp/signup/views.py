42d5e61f521f27410141ac9acaba4d0160d0c998
Vedant Puri <vedantpuri13@gmail.com>
2020-02-16 00:28:24 -0500

    Create dropdown of locations

--- weatherapp/signup/views.py
+++ weatherapp/signup/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Location
 
 # Create your views here.
 def index(request):
- 	return render(request, 'index.html', context={})
+    locations = Location.objects.all()
+    return render(request, 'index.html', context={"locations":locations})
